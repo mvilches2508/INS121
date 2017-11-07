a8140c0c6c5a87c4a3d3e8ce347ca282cf827662
fimar <fimar@MARIOV>
2017-11-07 20:56:41 -0300

    segunda carga con especificación de clase File

--- ClasesINS121/src/archivos/Menu.java
+++ ClasesINS121/src/archivos/Menu.java
@@ -6,10 +6,13 @@
 package archivos;
 
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -18,18 +21,32 @@ import java.util.ArrayList;
 public class Menu {
     
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         try {
-            File archivo = new File("src\\Usuarios\\usuariosBD.txt");
-            System.out.println("Esto es un directorio ? :"+ archivo.isDirectory());
+            File archivo = new File("src\\Usuarios\\usuariosBD.csv");
+            //Creación, uso e eliminación de una carpeta
+            File carpeta = new File("src\\Ejemplo1");
+            carpeta.mkdir();
+                    File archivoInterno = new File(carpeta.getPath()+"\\archivoInterno2.txt");
+                    archivoInterno.createNewFile();
+             
+            File[] archivos = carpeta.listFiles();
+            for (File arg : archivos) {
+                if(arg.isFile()){
+                    arg.delete();
+                }
+            }
+            carpeta.delete();
+            ///Propiedades de un archivo
+                    System.out.println("Esto es un directorio ? :"+ archivo.isDirectory());
             System.out.println("Se puede leer ? :"+ archivo.canRead());
             System.out.println("Se puede escribir? :"+ archivo.canWrite());
             System.out.println("Peso: "+ archivo.length());
             System.out.println("Nombre :"+ archivo.getName());
             System.out.println("ruta :"+ archivo.getPath());
             System.out.println("ruta absoluta: "+ archivo.getAbsolutePath());
-            
+            //Escritura de Archivos
             FileOutputStream fostm = new FileOutputStream(archivo);
             
             String info ="INFORMACION CARGADA EN TEXTO";
@@ -41,9 +58,8 @@ public class Menu {
             texto.append("\r\n Edgardo Harley Cardenas");
             texto.append("\r\n Alexis SANS");
             texto.append("\r\n El niño del pelo raro...PANCHO!");
-            
             if (archivo.exists()) {
-                
+                archivo.delete();
             } else {
                 archivo.createNewFile();
             }
@@ -51,7 +67,15 @@ public class Menu {
             fostm.write(contenido);
             fostm.flush();
             fostm.close();
-           
+            //Lectura de Archivos
+            FileReader fr = new FileReader(archivo);
+            BufferedReader br = new BufferedReader(fr);
+            String lectura;
+            while((lectura=br.readLine()) != null){
+                Thread.sleep(2000);
+                System.out.println(""+lectura);
+            }
+            
 
         } catch (IOException ex) {
             System.out.println("Error al manejar archivos: " + ex);
